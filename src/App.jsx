e1a892e3a8f8e9a6d43430da63bfa3d6c353887e
Marley <askellad4157@gmail.com>
2025-10-04 20:15:52 -0300

    feat: landing page completa e responsiva

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,17 @@ import Header from './components/Header/Header.jsx';
 import HeroSection from './features/HeroSection/HeroSection.jsx';
 import ToolsSection from './features/ToolsSection/ToolsSection.jsx';
 import FeaturesSection from './features/FeaturesSection/FeaturesSection.jsx';
-import Footer from './components/Footer/Footer.jsx'; // Importe o novo componente
+import Footer from './components/Footer/Footer.jsx';
 
 function App() {
   return (
     <div>
+      {/* O elemento .gradient-bg foi removido */}
       <Header />
       <HeroSection />
       <ToolsSection />
       <FeaturesSection />
-      <Footer /> {/* Adicione o novo componente aqui */}
+      <Footer />
     </div>
   );
 }
